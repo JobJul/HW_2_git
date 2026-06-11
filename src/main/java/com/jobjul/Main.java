d57cd20402a808ba2fa42e9edda4c320eecd82f7
JobJul <164882723+JobJul@users.noreply.github.com>
2026-06-11 23:35:41 +0300

    Update Main.java translated into Russian

--- src/main/java/com/jobjul/Main.java
+++ src/main/java/com/jobjul/Main.java
@@ -3,7 +3,7 @@ package com.jobjul;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Привет and welcome!");
+        System.out.printf("Привет и добро пожаловать!");
 
         for (int i = 1; i <= 5; i++) {
           System.out.println("i = " + i);
